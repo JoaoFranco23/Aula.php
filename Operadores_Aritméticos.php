41002f907a782635491ba27f2519ba0f2840354f
JoaoFranco23 <147751659+JoaoFranco23@users.noreply.github.com>
2023-11-05 09:46:39 -0300

    Update Operadores_Aritméticos.php

--- Operadores_Aritméticos.php
+++ Operadores_Aritméticos.php
@@ -4,3 +4,25 @@
         $s = $n1 + $n2;
         echo "$n1 + $n2 = $s";
     ?>
+
+<?php
+            $n1 = 4;
+            $n2 = 5; 
+            echo "A soma vale ". ( $n1+$n2 ); 
+            echo "<br/>A subtracao vale ". ( $n1-$n2 ); 
+            echo "<br/>A multiplicacao vale ". ( $n1*$n2 );
+            echo "<br/>A divisao vale ". ( $n1/$n2 );
+            echo "<br/>O modulo vale ". ( $n1%$n2 );   
+        ?>
+
+ <?php
+            $n1 = 4;
+            $n2 = 5;
+            $m = ($n1 + $n2) /2;
+            echo "A soma vale ". ( $n1+$n2 ); 
+            echo "<br/>A subtracao vale ". ( $n1-$n2 ); 
+            echo "<br/>A multiplicacao vale ". ( $n1*$n2 );
+            echo "<br/>A divisao vale ". ( $n1/$n2 );
+            echo "<br/>O modulo vale ". ( $n1%$n2 ); 
+            echo "<br/> A media vale $m";
+        ?>
