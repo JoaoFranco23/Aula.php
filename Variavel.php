e2b59eafe82e7b821be1f1c4f1215218d24d4485
JoaoFranco23 <147751659+JoaoFranco23@users.noreply.github.com>
2023-11-05 08:32:41 -0300

    Create Variavel.php

---
+++ Variavel.php
@@ -0,0 +1,14 @@
+<?php
+            $n = 4;
+            $nome = "Joao  Vitor";
+            $n = 4.5;
+            echo $n;
+        ?> 
+
+
+<?php //utilizando typecast pra converter
+            $n = 4;
+            $nome = (int)"Joao  Vitor";
+            $n = 4.5;
+            echo $nome;
+        ?>
