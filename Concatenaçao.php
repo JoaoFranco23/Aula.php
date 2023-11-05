6fc91f2e3911be14258b24945798536b114001d3
JoaoFranco23 <147751659+JoaoFranco23@users.noreply.github.com>
2023-11-05 09:03:35 -0300

    Update Concatenaçao.php

--- Concatenaçao.php
+++ Concatenaçao.php
@@ -3,3 +3,9 @@
             $idade = 23;
             echo $nome. " tem ". $idade. " anos! "
         ?>
+
+<?php //concatenação
+            $nome = "Mafalda";
+            $idade = 23;
+            echo "$nome tem $idade anos!"
+        ?>
